2b99eae60dc7c428cf3c53fadc03ef7f966dc1a3
Manoj Baishya <manojbaishya@users.noreply.github.com>
2025-03-04 17:32:05 +0530

    Fix controller

--- dojo.api/Todo/TodoController.cs
+++ dojo.api/Todo/TodoController.cs
@@ -10,14 +10,14 @@ public class TodoController(ITodoService todoService) : ControllerBase
     [HttpGet] public async Task<ActionResult<IEnumerable<Todo>>> GetTodoItems() => Ok(await todoService.GetTodoItems());
 
     // NOTE GET: /api/Todo/5
-    [HttpGet("{id:long}")] public async Task<ActionResult<Todo>> GetTodoItem(long id) => await todoService.GetTodoItemById(id) is { } todoItem ? Ok(todoItem) : NotFound();
+    [HttpGet("{id}")] public async Task<ActionResult<Todo>> GetTodoItem(long id) => await todoService.GetTodoItemById(id) is { } todoItem ? Ok(todoItem) : NotFound();
 
     // NOTE PUT: /api/Todo/5
-    [HttpPut("{id:long}")] public async Task<IActionResult> PutTodoItem(long id, TodoDto todoDto) => await todoService.UpdateTodoItem(id, todoDto) ? NoContent() : NotFound();
+    [HttpPut("{id}")] public async Task<IActionResult> PutTodoItem(long id, [FromBody] TodoDto todoDto) => await todoService.UpdateTodoItem(id, todoDto) ? NoContent() : NotFound();
 
     // NOTE POST: /api/Todo
-    [HttpPost] public async Task<ActionResult<Todo>> PostTodoItem(TodoDto todoDto) => CreatedAtAction(nameof(GetTodoItem), new { id = await todoService.CreateTodoItem(todoDto) }, todoDto);
+    [HttpPost] public async Task<ActionResult<Todo>> PostTodoItem([FromBody] TodoDto todoDto) => CreatedAtAction(nameof(GetTodoItem), new { id = await todoService.CreateTodoItem(todoDto) }, todoDto);
 
     // NOTE DELETE: /api/Todo/5
-    [HttpDelete("{id:long}")] public async Task<IActionResult> DeleteTodoItem(long id) => await todoService.DeleteTodoItem(id) ? NoContent() : NotFound();
+    [HttpDelete("{id}")] public async Task<IActionResult> DeleteTodoItem(long id) => await todoService.DeleteTodoItem(id) ? NoContent() : NotFound();
 }
